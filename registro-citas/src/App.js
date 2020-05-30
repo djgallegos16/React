28a25ad13694fb7e8d485b4ea2cda74082fb6983
Daniela Gallegos <gallegosdaniela16@hotmail.com>
2020-05-29 22:10:38 -0500

    Mostrar y eliminar citas

--- registro-citas/src/App.js
+++ registro-citas/src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState} from 'react';
 
 import Formulario from './components/Formulario'
+import Cita from './components/Cita'
 
 function App() {
 
@@ -13,6 +14,15 @@ function App() {
     guardarCitas([ ... citas, cita ])
   }
 
+  // Funcion que elimina una cita por el id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id != id)
+    guardarCitas(nuevasCitas);
+  }
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -24,7 +34,14 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            2
+            <h2>{titulo}</h2>
+            {citas.map(cita =>(
+              <Cita 
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
           </div>
         </div>
       </div>
